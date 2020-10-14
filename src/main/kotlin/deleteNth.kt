ecce42dafe8d8982b6b9df1fc3e028f57c82dfd1
Hipythea <my@abc.your.com>
2020-10-15 00:53:53 +0800

    Removed un-needed code and recommit
    See #1

--- src/main/kotlin/deleteNth.kt
+++ src/main/kotlin/deleteNth.kt
@@ -1,21 +1,6 @@
 import org.junit.Assert.assertArrayEquals
 
 fun deleteNth(elements:IntArray, maxOcurrences:Int):IntArray {
-    //val tempArray = elements
-//    val tempMap = mutableMapOf<Int, Int>()
-//    for ((index, value) in elements.withIndex()){
-//        tempMap[index] = value
-//    }
-//    for ((index,value) in tempMap){
-//        println ("Index is $index and value is $value")
-//    }
-//    if (tempMap.values.count() >= maxOcurrences) {
-//        println(tempMap.values.count())
-//    }
-//    val tempMap2 = tempMap.filterValues { it >= maxOcurrences }.values
-//    for (i in tempMap2){
-//        println ("i is $i")
-//    }
     val tempArray = mutableListOf<Int>()
     for (i in elements){
         if (tempArray.count {it == i} < maxOcurrences){
